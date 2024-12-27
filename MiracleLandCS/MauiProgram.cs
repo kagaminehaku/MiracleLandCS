348077f27a93222191c24127ff9eacc3d03a49bb
kagaminehaku <blomaway@gmail.com>
2024-12-27 21:36:14 +0700

    v2.0

--- MiracleLandCS/MauiProgram.cs
+++ MiracleLandCS/MauiProgram.cs
@@ -14,7 +14,7 @@ namespace MiracleLandCS
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
-            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5287/") });
+            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/") });
             builder.Services.AddSingleton<AuthService>();
             builder.Services.AddSingleton<RegisterService>();
             builder.Services.AddSingleton<UserService>();
