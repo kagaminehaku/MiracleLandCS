20bc2d62883bc5d66598fd5fe50c4598c2d4648c
kagaminehaku <blomaway@gmail.com>
2024-12-28 18:49:59 +0700

    Bug Fix

--- MiracleLandCS/MauiProgram.cs
+++ MiracleLandCS/MauiProgram.cs
@@ -14,7 +14,7 @@ namespace MiracleLandCS
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
-            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/") });
+            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http://kagaminehaku.softether.net:25565/") });
             builder.Services.AddSingleton<AuthService>();
             builder.Services.AddSingleton<RegisterService>();
             builder.Services.AddSingleton<UserService>();
